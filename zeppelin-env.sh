14d2135a294872f10b1fb528a56ec30423224934
Abhay Agarwal <abhay.agarwal@berkeley.edu>
2015-06-10 13:29:25 -0700

    added environment file

---
+++ zeppelin-env.sh
@@ -0,0 +1,4 @@
+export MASTER=mesos://leader.mesos:5050
+export ZEPPELIN_JAVA_OPTS="-Dspark.executor.uri=http://downloads.mesosphere.com.s3.amazonaws.com/assets/spark/spark-1.4.0-SNAPSHOT-bin-2.2.0.tgz"
+export SPARK_EXECUTOR_URI=http://downloads.mesosphere.com.s3.amazonaws.com/assets/spark/spark-1.4.0-SNAPSHOT-bin-2.2.0.tgz
+export MESOS_NATIVE_LIBRARY=/usr/lib/libmesos.so
